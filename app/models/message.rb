b2f3bff8445094048986fac30d57d059b8f241bd
Thiago-Cardoso <trovador182@gmail.com>
2018-03-17 11:40:15 -0300

    action_cable

--- app/models/message.rb
+++ app/models/message.rb
@@ -4,4 +4,5 @@ class Message < ApplicationRecord
     belongs_to :user
      #required field body in user when create a record message
     validates_presence_of :body, :user
+    after_create_commit {MessageBroadcastJob.perform_later self}
 end
