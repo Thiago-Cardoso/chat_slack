3cf57dd6c9398a4aea79d838a4d7c4150b85ab07
Thiago-Cardoso <trovador182@gmail.com>
2018-03-20 21:27:24 -0300

    fixed problem

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -2,17 +2,13 @@ class TeamsController < ApplicationController
   before_action :set_team, only: [:destroy]
   before_action :set_by_slug_team, only: [:show]
 
-  #list all teams of user logged
   def index
-    @teams = current_user.teams
   end
 
-  #verificate if user logged team have permit for read in record @team
   def show
     authorize! :read, @team
   end
 
-  #create a team with slug and user is logged
   def create
     @team = Team.new(team_params)
 
@@ -25,32 +21,26 @@ class TeamsController < ApplicationController
     end
   end
 
-  #test with can can user can destroy record of team (owner)
   def destroy
     authorize! :destroy, @team
     @team.destroy
 
     respond_to do |format|
       format.json { render json: true }
-      #format.html { redirect_to main_app.root_url, notice: 'Team deleted' }
     end
   end
 
   private
 
-  #search for slug
   def set_by_slug_team
     @team = Team.find_by(slug: params[:slug])
   end
 
-  #set team for id
   def set_team
     @team = Team.find(params[:id])
   end
 
-  #params permit
   def team_params
     params.require(:team).permit(:slug).merge(user: current_user)
   end
-
 end
--- app/models/channel.rb
+++ app/models/channel.rb
@@ -1,11 +1,8 @@
 class Channel < ApplicationRecord
-  #
   has_many :messages, as: :messagable, :dependent => :destroy
-  #has one user
-  belongs_to :user
-  #has one team
   belongs_to :team
-  #required a file slug user and team when create a record channel
+  belongs_to :user
   validates_presence_of :slug, :team, :user
-  validates :slug, uniqueness: true, format: { with: /\A[a-zA-Z0-9]+\Z/ }
+  validates :slug, format: { with: /\A[a-zA-Z0-9]+\Z/ }
+  validates_uniqueness_of :slug, :scope => :team_id
 end
